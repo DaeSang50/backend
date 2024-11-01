a202ec47af2c37d09ea01de0a62e29cd46103180
taeyeongKims <xodud8530@gmail.com>
2024-11-01 16:54:08 +0900

    init

---
+++ src/main/java/DaeSang/cheatList/CheatListApplication.java
@@ -0,0 +1,13 @@
+package DaeSang.cheatList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheatListApplication.class, args);
+	}
+
+}
